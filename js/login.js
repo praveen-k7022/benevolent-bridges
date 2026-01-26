f462cf657108fcb5433dbbf70a44f1ad271fc878
Praveen Kumar K <23bcar0117@jainuniversity.ac.in>
2026-01-26 15:59:53 +0530

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,13 @@
+function login() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    // Demo credentials
+    if (email === "kpraveenkumar7022@gmail.com" && password === "1234") {
+        alert("Login successful!");
+        window.location.href = "index.html";
+
+    } else {
+        alert("Invalid email or password");
+    }
+}
